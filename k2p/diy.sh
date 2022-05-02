1244aeb1ea498c5f37db3a62785dd5a198c6f94e
ylqjgm <ylqjgm@gmail.com>
2022-05-02 18:00:45 +0800

    fix: k2p

---
+++ k2p/diy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+valtime=$(TZ='Asia/Shanghai' date '+%Y-%m-%d %H:%M')
+val2="\\1 footer_code +='编译日期 $valtime by <a href=\"https://github.com/ylqjgm/Route\" target=\"blank\">ylqjgm</a>';"
+sed -i "s#\(.*Non-Commercial Use Only[^;]*;\).*#$val2#" /opt/padavan/trunk/user/www/n56u_ribbon_fixed/state.js
+grep "Non-Commercial Use Only" /opt/padavan/trunk/user/www/n56u_ribbon_fixed/state.js
