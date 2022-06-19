a7883d56c614c316a72cb7766a840179fd4f6b64
ylqjgm <ylqjgm@gmail.com>
2022-06-20 00:02:34 +0800

    chroe: add ImmortalWrt, change config

--- bypass/diy.sh
+++ bypass/diy.sh
@@ -1,41 +1,67 @@
 #!/bin/bash
 
-# 设置默认主题
-svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-sed -i 's#luci-theme-bootstrap#luci-theme-infinityfreedom#g' feeds/luci/collections/luci/Makefile
-
-# 修改默认IP
-sed -i 's#192.168.1.1#10.9.8.254#g' package/base-files/files/bin/config_generate
-
-# 修改版本号
-sed -i "s/OpenWrt /KangNuo build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-# 添加各种包
-git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/luci-app-autoipsetadder
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+# 添加插件
+rm -rf package/feeds/luci/luci-app-netdata
+mkdir -p package/ylqjgm
+pushd package/ylqjgm
+svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
+git clone https://github.com/sirpdboy/luci-app-advanced.git
+svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
+git clone https://github.com/sirpdboy/luci-app-netdata.git
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git
+git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git
 
 # 科学上网
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-plugin package/xray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocksr-libev package/shadowsocksr-libev
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs package/simple-obfs
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan package/trojan
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core package/v2ray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-plugin
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocks-rust
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocksr-libev
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy
+popd
+
+# 设置默认主题
+sed -i 's#luci-theme-bootstrap#luci-theme-infinityfreedom#g' feeds/luci/collections/luci/Makefile
+
+# 调整菜单
+sed -i 's/services/vpn/g' package/ylqjgm/luci-app-passwall/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' package/ylqjgm/luci-app-passwall/luasrc/model/cbi/passwall/*/*.lua
+sed -i 's/services/vpn/g' package/ylqjgm/luci-app-passwall/luasrc/view/passwall/*/*.htm
+
+# 修改makefile
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/lang\/golang\/golang\-package\.mk/include \$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang\-package\.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
+
+# 修改Hostname
+sed -i 's/OpenWrt/Bypass/g' package/base-files/files/bin/config_generate
+
+# 修改默认IP
+sed -i 's#192.168.1.1#10.9.8.2#g' package/base-files/files/bin/config_generate
+
+# 修改版本号
+sed -i "s/OpenWrt /ylqjgm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/${3:-LuCI}/ylqjgm/g' feeds/luci/modules/luci-base/src/mkversion.sh
+sed -i 's/${2:-Git}/$(TZ=UTC-18 date "+%Y-%m-%d")/g' feeds/luci/modules/luci-base/src/mkversion.sh
+
+# 修改插件名字
+sed -i 's/"TTYD 终端"/"终端"/g' `grep "TTYD 终端" -rl ./`
+sed -i 's/"挂载 SMB 网络共享"/"挂载共享"/g' `grep "挂载 SMB 网络共享" -rl ./`
--- immortalwrt/diy.sh
+++ immortalwrt/diy.sh
@@ -5,9 +5,9 @@ rm -rf package/feeds/luci/luci-app-netdata
 mkdir -p package/ylqjgm
 pushd package/ylqjgm
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
-git clone https://github.com/sirpdboy/luci-app-advanced.git
+svn co https://github.com/messense/aliyundrive-fuse/trunk/openwrt/aliyundrive-fuse
+svn co https://github.com/messense/aliyundrive-fuse/trunk/openwrt/luci-app-aliyundrive-fuse
 git clone https://github.com/jerrykuku/luci-app-ttnode.git
-git clone https://github.com/yaof2/luci-app-ikoolproxy.git
 svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
 git clone https://github.com/sirpdboy/luci-app-netdata.git
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git
@@ -17,9 +17,9 @@ popd
 sed -i 's#luci-theme-bootstrap#luci-theme-infinityfreedom#g' feeds/luci/collections/luci/Makefile
 
 # 调整菜单
-sed -i 's/services/nas/g' feeds/luci/applications/luci-app-aliyundrive-fuse/luasrc/controller/*.lua
-sed -i 's/services/nas/g' feeds/luci/applications/luci-app-aliyundrive-fuse/luasrc/model/cbi/aliyundrive-fuse/*.lua
-sed -i 's/services/nas/g' feeds/luci/applications/luci-app-aliyundrive-fuse/luasrc/view/aliyundrive-fuse/*.htm
+sed -i 's/services/nas/g' package/ylqjgm/luci-app-aliyundrive-fuse/luasrc/controller/*.lua
+sed -i 's/services/nas/g' package/ylqjgm/luci-app-aliyundrive-fuse/luasrc/model/cbi/aliyundrive-fuse/*.lua
+sed -i 's/services/nas/g' package/ylqjgm/luci-app-aliyundrive-fuse/luasrc/view/aliyundrive-fuse/*.htm
 
 # 修改makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
@@ -27,16 +27,23 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
 
-# 修改Hostname
-sed -i 's/OpenWrt/Primary/g' package/base-files/files/bin/config_generate
-
 # 修改默认IP
 sed -i 's#192.168.1.1#10.9.8.1#g' package/base-files/files/bin/config_generate
 
+# 修改Hostname
+sed -i 's/ImmortalWrt/Primary/g' package/base-files/files/bin/config_generate
+
 # 修改版本号
-sed -i "s/OpenWrt /ylqjgm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='ylqjgm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt '" >> package/base-files/files/etc/openwrt_release
 sed -i 's/${3:-LuCI}/ylqjgm/g' feeds/luci/modules/luci-base/src/mkversion.sh
 sed -i 's/${2:-Git}/$(TZ=UTC-18 date "+%Y-%m-%d")/g' feeds/luci/modules/luci-base/src/mkversion.sh
+
+# 修正连接数
+sed -i '1i net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/emortal/default-settings/files/99-default-settings
 
 # 修改插件名字
 sed -i 's/"Aria2 配置"/"Aria2"/g' `grep "Aria2 配置" -rl ./`
