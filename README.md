69cc1cb9734745cce3733b5164ccce737d7ca9b2
ylqjgm <ylqjgm@gmail.com>
2022-06-28 00:17:30 +0800

    chroe: 去除ImmortalWrt，去除主路由，旁路由配置更新

--- README.md
+++ README.md
@@ -4,25 +4,37 @@
 
 组网说明：
 
-R86S安装PVE虚拟双路由，主路由（Primary）桥接光猫拨号上网，旁路由（Bypass）负责科学上网，K2P作为AP使用。
+R86S安装PVE虚拟iKuai作为主路由，负责拨号上网，虚拟OpenWRT作为旁路由，负责广告过滤、科学上网等，K2P作为AP使用。
 
-**固件通过获取lede源码来确定是否更新**
+**K2P使用Padavvan默认配置**
 
-### 主路由配置
+### OpenWRT 配置
 
-1. 登录地址：`10.9.8.1`
-2. 登录帐号：`root`
-3. 登录密码：`password`
+| 登录地址 | 默认帐号 | 默认密码 |
+| ---- | ---- | ---- |
+| 10.9.8.2 | root | password |
 
-### 旁路由配置
+* 插件列表
 
-1. 登录地址：`10.9.8.2`
-2. 登录帐号：`root`
-3. 登录密码：`password`
-
-### K2P配置
-
-Padavan默认配置
+| 插件 | 说明 |
+| ---- | ---- |
+| luci-app-advanced | 高级配置 |
+| luci-app-autoreboot | 定时重启 |
+| luci-app-ttnode | 甜糖星愿自动采集 |
+| luci-app-aliyundrive-fuse | 阿里云盘 FUSE |
+| luci-app-adguardhome | AdGuard Home 广告过滤 |
+| luci-app-pushbot | 钉钉推送 |
+| luci-app-openclash | OpenClash 分流、科学上网 |
+| luci-app-ddns | 动态域名 |
+| luci-app-vlmcsd | KMS 服务器 |
+| luci-app-docker | Docker容器 |
+| luci-app-dockerman | Docker管理 |
+| luci-app-zerotier | 异地组网 |
+| luci-app-aria2 | Aria2下载 |
+| luci-app-baidupcs-web | 百度网盘 |
+| luci-app-tcpdump | 网络抓包 |
+| luci-app-netspeedtest | 网速测试 |
+| luci-theme-infinityfreedom | infinityfreedom主题 |
 
 ## 感谢
 
---
+++ openwrt/diy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# 添加插件
+rm -rf package/feeds/luci/luci-app-netdata
+rm -rf package/feeds/luci/luci-app-baidupcs-web
+mkdir -p package/ylqjgm
+pushd package/ylqjgm
+svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
+git clone https://github.com/sirpdboy/luci-app-advanced.git
+git clone https://github.com/zzsj0928/luci-app-pushbot.git
+svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
+git clone https://github.com/sirpdboy/luci-app-netdata.git
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git
+git clone https://github.com/kiddin9/luci-app-baidupcs-web.git
+git clone https://github.com/jerrykuku/luci-app-ttnode.git
+svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
+git clone https://github.com/vernesong/OpenClash.git
+popd
+
+# 设置默认主题
+sed -i 's#luci-theme-bootstrap#luci-theme-infinityfreedom#g' feeds/luci/collections/luci/Makefile
+
+# 修复糖甜
+sed -i 's|src="/ttnode/jquery.min.js|src="/luci-static/ttnode/jquery.min.js|' package/ylqjgm/luci-app-ttnode/luasrc/view/ttnode/login_form.htm
+sed -i '/if.*(d.error == 0)/{n;s/settime()/countdown = 60\;\n\t\t\t\t\t&/g}' package/ylqjgm/luci-app-ttnode/luasrc/view/ttnode/login_form.htm
+sed -i '/jq.cookie(.ltime., 0)/{n;s/countdown = 60/\/\/&/g}' package/ylqjgm/luci-app-ttnode/luasrc/view/ttnode/login_form.htm
+
+# 修复百度云盘启动项
+rm -f package/ylqjgm/luci-app-baidupcs-web/root/etc/init.d/baidupcs-web
+cp baidupcs-web package/ylqjgm/luci-app-baidupcs-web/root/etc/init.d/baidupcs-web
+
+# 修改makefile
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/lang\/golang\/golang\-package\.mk/include \$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang\-package\.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
+
+# 修改默认IP
+sed -i 's#192.168.1.1#10.9.8.2#g' package/base-files/files/bin/config_generate
+
+# 修改版本号
+sed -i "s/OpenWrt /ylqjgm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/${3:-LuCI}/ylqjgm/g' feeds/luci/modules/luci-base/src/mkversion.sh
+sed -i 's/${2:-Git}/$(TZ=UTC-18 date "+%Y-%m-%d")/g' feeds/luci/modules/luci-base/src/mkversion.sh
